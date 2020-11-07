b79da517b07cba398891e69c9872d814d944b3a4
developerdevikumar <developerdevikumar@hmail.com>
2020-11-07 19:34:26 +0530

    addlog

---
+++ auth/DB.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB: 'mongodb://localhost:27017/task-rolled',
+    secret: 'kumar-token'
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthModule } from './auth/auth.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
